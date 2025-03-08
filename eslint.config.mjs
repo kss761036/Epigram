6598b6473b74e1844c67b43637938de31949a5ef
김승석 <kss53043@naver.com>
2025-03-09 03:57:54 +0900

    :bug: Fix: 깃 세팅 수정 및 라이브러리 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
   {
@@ -20,9 +18,39 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     rules: {
-      'prettier/prettier': ' off',
+      'prettier/prettier': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      'no-warning-comments': [
+        'error',
+        {
+          terms: [
+            '✨',
+            '✅',
+            '🔧',
+            '🚀',
+            '💡',
+            '❗',
+            '🔍',
+            '🔥',
+            '⭕',
+            '🔵',
+            '⚪',
+            '🟢',
+            '🔴',
+            '🟡',
+            '🟠',
+            '📝',
+            '🚧',
+            '⚙️',
+            '💾',
+            '📌',
+            '📍',
+            '🔗',
+          ],
+          location: 'anywhere',
+        },
+      ],
     },
   },
 ];
