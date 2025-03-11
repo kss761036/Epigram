83006acdcfd694d75f6fb202c36606c7b3c59ef3
SeokChan-Lee <melee0826@gmail.com>
2025-03-12 01:50:34 +0900

    :lipstick: Design: textarea 컴포넌트 반응형 수정 & required prop 추가

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -4,26 +4,39 @@ import { useId } from 'react';
 
 interface TextAreaProps {
   error?: string;
+  errorClassName?: string;
   className?: string;
   placeholder?: string;
   label?: string;
   labelClassName?: string;
+  required?: boolean;
 }
 
 export default function TextArea({
   error,
+  errorClassName,
   className,
   placeholder,
   label,
   labelClassName,
+  required,
   ...props
 }: TextAreaProps) {
   const id = useId();
   return (
-    <div className='flex flex-col gap-2'>
+    <div className='flex flex-col'>
       {label && (
-        <label htmlFor={id} className={`${labelClassName}`}>
+        <label
+          htmlFor={id}
+          className={cn(
+            'text-black-600 mb-[9px] text-lg font-semibold md:mb-2.5 md:text-lg lg:mb-[27px] lg:text-xl',
+            labelClassName,
+          )}
+        >
           {label}
+          {required && (
+            <span className='text-red ml-1 text-lg md:text-lg lg:ml-1.5 lg:text-xl'>*</span>
+          )}
         </label>
       )}
       <textarea
@@ -36,7 +49,16 @@ export default function TextArea({
         )}
         {...props}
       />
-      {error && <span className='text-red text-right'>{error}</span>}
+      {error && (
+        <span
+          className={cn(
+            'text-red md:text-md mt-2 text-right text-xs lg:mt-4 lg:text-lg',
+            errorClassName,
+          )}
+        >
+          {error}
+        </span>
+      )}
     </div>
   );
 }
