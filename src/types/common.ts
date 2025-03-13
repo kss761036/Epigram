89c62a3464838d88d8a226237c122a48ddbd272f
chanki.kim <chanki.kim89@gmail.com>
2025-03-13 16:24:03 +0900

    ✨ Feat: 감정 한글라벨 정의

--- src/types/common.ts
+++ src/types/common.ts
@@ -6,4 +6,12 @@ export const EMOTION = {
   ANGRY: 'ANGRY',
 } as const;
 
+export const EMOTION_LABEL: Record<Emotion, string> = {
+  MOVED: '감동',
+  HAPPY: '기쁨',
+  WORRIED: '고민',
+  SAD: '슬픔',
+  ANGRY: '분노',
+};
+
 export type Emotion = (typeof EMOTION)[keyof typeof EMOTION];
