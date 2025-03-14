b958d04044024d5a7fa1170cf83fbdf003fb8293
김승석 <kss53043@naver.com>
2025-03-15 01:29:35 +0900

    📦️ Chore: 댓글 컴포넌트 stories 수정

--- src/components/Comment.stories.ts
+++ src/components/Comment.stories.ts
@@ -22,7 +22,7 @@ const meta: Meta<typeof Comment> = {
       control: 'text',
     },
 
-    author: {
+    writer: {
       description: '댓글 작성자 정보 (프로필이미지, 닉네임)',
       table: {
         type: { summary: 'object' },
@@ -71,7 +71,7 @@ export const Default: Story = {
   args: {
     content:
       '이 세상에는 위대한 진실이 하나 있어. 무언가를 온 마음을 다해 원한다면, 반드시 그렇게 된다는 거야. 무언가를 바라는 마음은 곧 우주의 마음으로부터 비롯된 것이기 때문이지.',
-    author: { image: 'https://placehold.co/40x40/000000/FFFFFF.png', nickname: '파울로 코엘료' },
+    writer: { image: 'https://placehold.co/40x40/000000/FFFFFF.png', nickname: '파울로 코엘료' },
     createdAt: '2025-03-14T12:04:09.521Z',
     className: '',
     handleEdit: () => alert('수정 버튼 클릭'),
