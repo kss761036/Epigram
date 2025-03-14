de89b10a0e814af711684677ab9ba640e6cf950a
SeokChan-Lee <melee0826@gmail.com>
2025-03-14 21:25:52 +0900

    Feat/67/캘린더컴포넌트 (#68)
    * :package: Chore: 패키지 추가(date-fns)
    
    * :sparkles: Feat: 캘린더 컴포넌트 구현
    
    * :books: Docs: 캘린더 컴포넌트 스토리북 추가
    
    * :white_check_mark: Test: 캘린더 컴포넌트 테스트 추가

---
+++ src/components/Calendar.stories.ts
@@ -0,0 +1,73 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Calendar from './Calendar';
+import { Emotion } from '@/types/common';
+
+const sampleMoodData: { [dateString: string]: Emotion } = {
+  '2025-02-23': 'MOVED',
+  '2025-02-24': 'MOVED',
+  '2025-02-25': 'MOVED',
+  '2025-02-26': 'WORRIED',
+  '2025-02-27': 'ANGRY',
+  '2025-02-28': 'ANGRY',
+  '2025-03-01': 'HAPPY',
+  '2025-03-03': 'HAPPY',
+  '2025-03-05': 'WORRIED',
+  '2025-03-07': 'SAD',
+  '2025-03-09': 'SAD',
+  '2025-03-10': 'SAD',
+  '2025-03-11': 'SAD',
+  '2025-03-12': 'ANGRY',
+  '2025-03-13': 'ANGRY',
+  '2025-03-15': 'ANGRY',
+  '2025-03-17': 'ANGRY',
+  '2025-03-19': 'ANGRY',
+  '2025-03-21': 'ANGRY',
+  '2025-03-22': 'ANGRY',
+  '2025-03-23': 'ANGRY',
+  '2025-03-24': 'ANGRY',
+  '2025-03-25': 'ANGRY',
+  '2025-03-26': 'ANGRY',
+  '2025-03-27': 'ANGRY',
+  '2025-03-28': 'ANGRY',
+  '2025-03-29': 'ANGRY',
+  '2025-03-30': 'ANGRY',
+  '2025-04-01': 'ANGRY',
+  '2025-04-02': 'ANGRY',
+  '2025-04-03': 'ANGRY',
+  '2025-04-04': 'ANGRY',
+  '2025-04-05': 'ANGRY',
+};
+
+const meta: Meta<typeof Calendar> = {
+  component: Calendar,
+  title: 'Components/Calendar',
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: '캘린더 컴포넌트',
+      },
+    },
+  },
+  argTypes: {
+    moodData: {
+      control: 'object',
+      description: '월별 감정 데이터.',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Calendar>;
+
+export const Default: Story = {
+  args: {
+    moodData: {},
+  },
+};
+
+export const WithMoodData: Story = {
+  args: {
+    moodData: sampleMoodData,
+  },
+};
