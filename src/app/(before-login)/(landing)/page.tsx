985c8595374015a3efc247864d6cacecd6fee72d
chanki.kim <chanki.kim89@gmail.com>
2025-03-15 21:20:31 +0900

    ✨ Feat: 랜딩에 임시 세션 체크 추가

--- src/app/(before-login)/(landing)/page.tsx
+++ src/app/(before-login)/(landing)/page.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+
 export default function LandingPage() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className='grid justify-center gap-4 p-4 text-center'>
       <h1 className='text-3xl'>랜딩페이지</h1>
