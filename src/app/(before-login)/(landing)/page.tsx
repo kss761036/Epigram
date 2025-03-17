e6c2081881f088f713d56f10473886a2eca9c9f4
SeokChan-Lee <melee0826@gmail.com>
2025-03-18 00:49:22 +0900

    :lipstick: Design: 임시네비게이션바 반응형 추가

--- src/app/(before-login)/(landing)/page.tsx
+++ src/app/(before-login)/(landing)/page.tsx
@@ -7,7 +7,7 @@ export default function LandingPage() {
   return (
     <>
       <div className='fixed top-0 z-10 w-full border-b-1 border-[#d7d7d7]'>
-        <div className='flex h-[52px] items-center justify-between'>
+        <div className='flex h-[52px] items-center justify-between md:h-[60px] lg:h-[80px]'>
           {/*TODO 임시 네비게이션바*/}
           <Icon name='search' />
           <span>Epigram</span>
