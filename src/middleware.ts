5b184fb8ea243b33a9442cae6d7bde0832e684a8
chanki.kim <chanki.kim89@gmail.com>
2025-03-17 16:47:15 +0900

    ✨ Feat: 미들웨어 원복 (nextauth가 통제하도록)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { refreshAccessToken } from './utils/auth';
+import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const sessionToken = req.cookies.get('next-auth.session-token')?.value;
-
-  const kakaoToken = req.cookies.get('accessToken')?.value;
-  const refreshToken = req.cookies.get('refreshToken')?.value;
-
-  if (kakaoToken && refreshToken) {
-    const currentTime = Date.now();
-    const tokenExpiry = Number(req.cookies.get('accessTokenExpires')?.value) || 0;
-
-    if (currentTime > tokenExpiry) {
-      const newAccessToken = await refreshAccessToken(refreshToken);
-
-      if (newAccessToken) {
-        const response = NextResponse.next();
-        response.cookies.set('accessToken', newAccessToken);
-        response.cookies.set('accessTokenExpires', (currentTime + 30 * 60 * 1000).toString());
-
-        return response;
-      } else {
-        return NextResponse.redirect(new URL('/login', req.url));
-      }
-    }
-  }
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   const protectedRoutes = ['/addepigram', '/epigrams', '/epigrams/[id]', '/mypage', '/search'];
 
   if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
-    if (!sessionToken && !kakaoToken) {
+    if (!token) {
       return NextResponse.redirect(new URL('/login', req.url));
     }
   }
