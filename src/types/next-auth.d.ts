90b3cafddf3905cbbf231f226924af32096b80a5
chanki.kim <chanki.kim89@gmail.com>
2025-03-17 17:10:38 +0900

    ✨ Feat: nextauth User 타입 확장

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,7 +1,8 @@
 import NextAuth from 'next-auth';
-
 declare module 'next-auth' {
   interface User {
+    error?: string;
+    nickname: User;
     accessToken: string;
     refreshToken: string;
   }
