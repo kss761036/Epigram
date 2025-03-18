f9334ae0a10120254563defec49c71dba8e13c20
chanki.kim <chanki.kim89@gmail.com>
2025-03-18 19:15:59 +0900

    ✨ Feat: 유저 서비스, 타입 추가

---
+++ src/apis/user/user.type.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+export type User = {
+  image: string | null;
+  updatedAt: string;
+  createdAt: string;
+  teamId: string;
+  nickname: string;
+  id: number;
+};
+
+const ACCEPT_FILE_TYPE = ['image/jpeg', 'image/jpg', 'image/png'];
+const FILE_LIMIT_MB = 2;
+
+export const updateUserFormSchema = z.object({
+  nickname: z
+    .string()
+    .min(1, '닉네임은 필수 입력입니다.')
+    .max(20, '닉네임은 최대 20자까지 가능합니다.'),
+
+  image: z
+    .union([
+      z.string().url(),
+      z
+        .instanceof(File)
+        .refine((file) => ACCEPT_FILE_TYPE.includes(file.type), {
+          message: '지원되지 않는 이미지 파일입니다',
+        })
+        .refine((file) => file.size < FILE_LIMIT_MB * 1024 * 1024, {
+          message: `${FILE_LIMIT_MB}MB 이하로 올려주세요`,
+        }),
+    ])
+    .optional()
+    .nullable(),
+});
+
+export type UpdateUserForm = z.infer<typeof updateUserFormSchema>;
