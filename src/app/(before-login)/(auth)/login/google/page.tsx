119635ae7e188895b2e6a2eaa9e8fa1cdff442be
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-03-19 01:24:33 +0900

    Bug/96/소셜로그인버그수정 (#97)
    * 🔥 Remove: 콜백 라우트 핸들러 삭제
    
    * ✨ Feat: 클라이언트 페이지 원복
    
    * ✨ Feat: 소셜로그인에 사용하는 env를 서버전용으로 수정

---
+++ src/app/(before-login)/(auth)/login/google/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+import { redirect, useSearchParams } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+
+export default function GoogleCallback() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+
+  useEffect(() => {
+    (async function () {
+      const res = await signIn('google', {
+        code,
+        redirect: false,
+      });
+
+      if (res?.ok) {
+        redirect('/');
+      } else {
+        redirect('/login?error=google login error');
+      }
+    })();
+  }, [code]);
+
+  return <div className='flex h-dvh items-center justify-center'>구글 로그인 중...</div>;
+}
