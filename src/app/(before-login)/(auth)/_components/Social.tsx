42d636a093846565d7458b522b17baa47a87a6f2
chanki.kim <chanki.kim89@gmail.com>
2025-03-19 11:28:40 +0900

    ✨ Feat: 소셜 인증 공용 컴포넌트 작업

---
+++ src/app/(before-login)/(auth)/_components/Social.tsx
@@ -0,0 +1,29 @@
+import { HTMLAttributes, PropsWithChildren } from 'react';
+import { cn } from '@/utils/helper';
+
+export function SocialList({ children }: PropsWithChildren) {
+  return <div className='flex items-center justify-center gap-4'>{children}</div>;
+}
+
+export function SocialHeader({ children }: PropsWithChildren) {
+  return (
+    <div className='relative mb-6 flex items-center justify-center text-xs after:absolute after:top-1/2 after:left-0 after:h-[1px] after:w-full after:bg-gray-100 after:content-[""] lg:mb-10 lg:text-xl'>
+      <span className='bg-bg relative z-1 px-[14px] text-blue-400 lg:px-6'>{children}</span>
+    </div>
+  );
+}
+
+export function SocialButton({
+  className,
+  ...props
+}: PropsWithChildren<HTMLAttributes<HTMLButtonElement>>) {
+  return (
+    <button
+      className={cn(
+        'relative aspect-square w-10 cursor-pointer items-center justify-center rounded-sm border border-gray-100 lg:w-[60px]',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
