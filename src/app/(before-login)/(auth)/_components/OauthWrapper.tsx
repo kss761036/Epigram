0244fc8ffe6151d88f976609224636f1d931385e
chanki.kim <chanki.kim89@gmail.com>
2025-03-19 11:28:13 +0900

    ✨ Feat: 소셜 인증 리다이렉트 페이지 컴포넌트 작업

---
+++ src/app/(before-login)/(auth)/_components/OauthWrapper.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from 'react';
+
+export default function OauthWrapper({ children }: PropsWithChildren) {
+  return <div className='bg-bg flex min-h-dvh items-center justify-center'>{children}</div>;
+}
+
+export function OauthLoading({ children }: PropsWithChildren) {
+  return <div className='flex flex-col items-center justify-center gap-8'>{children}</div>;
+}
+
+export function OauthIcon({ children }: PropsWithChildren) {
+  return (
+    <div className='relative'>
+      <div className='relative z-1 aspect-square w-20 rounded-full bg-white'>{children}</div>
+      <span className='absolute top-1/2 left-1/2 h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2 animate-ping rounded-full bg-blue-400 opacity-75'></span>
+    </div>
+  );
+}
+
+export function OauthMessage({ children }: PropsWithChildren) {
+  return <div className='text-center text-blue-400'>{children}</div>;
+}
