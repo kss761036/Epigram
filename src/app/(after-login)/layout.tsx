b820a2d38f44e267418548defe2b3d245313c7fd
chanki.kim <chanki.kim89@gmail.com>
2025-03-20 21:00:16 +0900

    ✨ Feat: 헤더컴포넌트 클라이언트 세션 사용으로 변경

--- src/app/(after-login)/layout.tsx
+++ src/app/(after-login)/layout.tsx
@@ -1,14 +1,10 @@
 import { PropsWithChildren, Suspense } from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/utils/nextauth';
 import AfterLoginHeader from '@/components/headers/AfterLogin';
 
-export default async function layout({ children }: PropsWithChildren) {
-  const session = await getServerSession(authOptions);
-
+export default function layout({ children }: PropsWithChildren) {
   return (
     <div className='pt-13 md:pt-15.5 lg:pt-20'>
-      <AfterLoginHeader session={session} />
+      <AfterLoginHeader />
       <Suspense>{children}</Suspense>
     </div>
   );
