2ea32909db1b47788cc884313a8f7c0afb4e5da3
chanki.kim <chanki.kim89@gmail.com>
2025-03-20 12:25:07 +0900

    ✨ Feat: 검색페이지 검색결과 컴포넌트 작업

---
+++ src/app/(after-login)/search/_components/SearchResult.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useEpigramSearchInfiniteQuery } from '@/apis/epigram/epigram.queries';
+import SearchCard from '@/components/SearchCard';
+import Spinner from '@/components/Spinner';
+
+interface SearchResultProps {
+  keyword?: string;
+}
+
+export default function SearchResult({ keyword }: SearchResultProps) {
+  const [ref, inView] = useInView();
+  const { data, fetchNextPage, hasNextPage, isFetching } = useEpigramSearchInfiniteQuery({
+    limit: 6,
+    keyword,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching) fetchNextPage();
+  }, [inView, hasNextPage, fetchNextPage, isFetching]);
+
+  const results = data?.pages.flatMap((page) => page.list) ?? [];
+  const isShowLoader = isFetching;
+  const isShowMoreTrigger = !isFetching && hasNextPage;
+  const isShowEmpty = keyword && !results.length && !isFetching;
+
+  if (isShowEmpty) {
+    return <div className='p-4 text-center text-blue-400'>검색결과가 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <ul>
+        {results.map((item) => (
+          <li key={item.id}>
+            <SearchCard {...item} keyword={keyword} link={`/epigrams/${item.id}`} />
+          </li>
+        ))}
+      </ul>
+
+      {isShowLoader && (
+        <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
+          <Spinner className='fill-black text-gray-100' />
+          검색중입니다.
+        </div>
+      )}
+
+      {isShowMoreTrigger && <div ref={ref}></div>}
+    </>
+  );
+}
