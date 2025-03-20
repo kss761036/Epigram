b064bf81ff07acc5150b1bf996f744633b052241
chanki.kim <chanki.kim89@gmail.com>
2025-03-20 16:02:59 +0900

    ✨ Feat: afterlogin layout 임포트 정리

--- src/app/(after-login)/layout.tsx
+++ src/app/(after-login)/layout.tsx
@@ -1,7 +1,7 @@
+import { PropsWithChildren, Suspense } from 'react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/nextauth';
 import AfterLoginHeader from '@/components/headers/AfterLogin';
-import { PropsWithChildren, Suspense } from 'react';
 
 export default async function layout({ children }: PropsWithChildren) {
   const session = await getServerSession(authOptions);
