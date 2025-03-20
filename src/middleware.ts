b3acc462fd97c73336b7803cbc2883e4fc342bde
chanki.kim <chanki.kim89@gmail.com>
2025-03-20 20:37:40 +0900

    ✨ Feat: 미들웨어 코드 개선 (불필요한 코드 제거 및 feeds 추가)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,8 @@ import { getToken } from 'next-auth/jwt';
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  const protectedRoutes = ['/addepigram', '/epigrams', '/epigrams/[id]', '/mypage', '/search'];
-
-  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 
   return NextResponse.next();
@@ -19,8 +15,8 @@ export const config = {
   matcher: [
     '/addepigram/:path*',
     '/epigrams/:path*',
-    '/epigrams/[id]/:path*',
     '/mypage/:path*',
     '/search/:path*',
+    '/feeds/:path*',
   ],
 };
