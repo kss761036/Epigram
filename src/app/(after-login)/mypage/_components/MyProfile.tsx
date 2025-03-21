d24fbe53ba04dad9b04578bf7bb9fe87b08a1720
LMS10 <ggb7410@naver.com>
2025-03-21 18:34:24 +0900

    ✨ Feat: 마이프로필 컴포넌트 구현

---
+++ src/app/(after-login)/mypage/_components/MyProfile.tsx
@@ -0,0 +1,37 @@
+import { signOut, useSession } from 'next-auth/react';
+import Avatar from '@/components/Avatar';
+
+export default function MyProfile() {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div className='relative'>
+        <div className='absolute top-[26px] left-1/2 z-3 flex -translate-x-1/2 flex-col items-center lg:top-[68px]'>
+          <div className='flex flex-col justify-center gap-4 lg:gap-6'>
+            <div className='flex flex-col items-center gap-2 lg:gap-4'>
+              {session && (
+                <>
+                  <Avatar
+                    src={session.user?.image ?? undefined}
+                    alt={session.user?.nickname}
+                    className='h-20 w-20 border-2 border-blue-300 text-xl lg:h-[120px] lg:w-[120px] lg:text-3xl'
+                  />
+                  <p className='text-black-950 text-lg font-medium lg:text-2xl'>
+                    {session.user?.nickname}
+                  </p>
+                </>
+              )}
+            </div>
+            <button
+              onClick={() => signOut()}
+              className='bg-line-100 text-md cursor-pointer rounded-[100px] px-3.5 py-1.5 text-gray-300 lg:px-4 lg:py-2 lg:text-xl lg:font-medium'
+            >
+              로그아웃
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
