9a19c9a6a85e5ba0ab19edbbeca8f59056cd4c91
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-03-24 17:25:36 +0900

    Bug/163/로컬스토리지key값변경 (#164)
    * 🐛 Bug: 최근검색어 키 수정
    
    * 🐛 Bug: 리스트타입 키 수정

--- src/store/feedsListTypeStore.ts
+++ src/store/feedsListTypeStore.ts
@@ -17,6 +17,6 @@ export const useFeedsListTypeStore = create(
         });
       },
     }),
-    { name: 'feedslistType' },
+    { name: 'feeds-list-type' },
   ),
 );
--- src/store/recentSearchStore.ts
+++ src/store/recentSearchStore.ts
@@ -24,6 +24,6 @@ export const useRecentSearchStore = create(
       },
       clear: () => set({ keywords: [] }),
     }),
-    { name: 'recentKeywords' },
+    { name: 'recent-keywords' },
   ),
 );
