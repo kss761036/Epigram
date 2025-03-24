bec563d60fcec080a034517da3a5b3b58504082f
chanki.kim <chanki.kim89@gmail.com>
2025-03-24 14:15:33 +0900

    ✨ Feat: 에피그램 작성 버튼 추가 (feeds 페이지)

--- src/app/(after-login)/feeds/page.tsx
+++ src/app/(after-login)/feeds/page.tsx
@@ -9,6 +9,7 @@ import EtcButton from '@/components/EtcButton';
 import Icon from '@/components/Icon';
 import { Section, SectionTitle, SectionUtil } from '@/components/Section';
 import Spinner from '@/components/Spinner';
+import FloatingButtons from '../_components/FloatingButtons';
 
 export default function FeedPage() {
   const { data, isFetching, hasNextPage, fetchNextPage } = useEpigramInfiniteQuery({ limit: 6 });
@@ -21,72 +22,75 @@ export default function FeedPage() {
   const isShowEnd = !isFetching && !hasNextPage;
 
   return (
-    <div className='bg-bg min-h-dvh py-8'>
-      <div className='mx-auto px-6 md:px-19 lg:max-w-7xl'>
-        <Section>
-          <SectionTitle>피드</SectionTitle>
-          <SectionUtil>
-            <button
-              className='cursor-pointer text-gray-200 transition-colors hover:text-gray-800 md:hidden'
-              onClick={toggleList}
+    <>
+      <div className='bg-bg min-h-dvh py-8'>
+        <div className='mx-auto px-6 md:px-19 lg:max-w-7xl'>
+          <Section>
+            <SectionTitle>피드</SectionTitle>
+            <SectionUtil>
+              <button
+                className='cursor-pointer text-gray-200 transition-colors hover:text-gray-800 md:hidden'
+                onClick={toggleList}
+              >
+                {listType === 'list' ? <Icon name='dashboard' /> : <Icon name='filter' />}
+              </button>
+            </SectionUtil>
+          </Section>
+          <div className='mb-50 md:mb-14'>
+            <ul
+              className={cn(
+                'grid grid-cols-2 items-start gap-6 md:grid-cols-2 md:gap-8',
+                listType === 'list' && 'grid-cols-1',
+              )}
             >
-              {listType === 'list' ? <Icon name='dashboard' /> : <Icon name='filter' />}
-            </button>
-          </SectionUtil>
-        </Section>
-        <div className='mb-50 md:mb-14'>
-          <ul
-            className={cn(
-              'grid grid-cols-2 items-start gap-6 md:grid-cols-2 md:gap-8',
-              listType === 'list' && 'grid-cols-1',
+              <AnimatePresence>
+                {results.map((feed) => (
+                  <motion.li
+                    layout
+                    key={feed.id}
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    <Card {...feed} />
+                  </motion.li>
+                ))}
+              </AnimatePresence>
+            </ul>
+
+            {isShowEmpty && (
+              <div className='flex items-center justify-center p-10 text-blue-400'>
+                작성된 에피그램이 없습니다.
+              </div>
             )}
-          >
-            <AnimatePresence>
-              {results.map((feed) => (
-                <motion.li
-                  layout
-                  key={feed.id}
-                  animate={{ opacity: 1 }}
-                  initial={{ opacity: 0 }}
-                  exit={{ opacity: 0 }}
-                >
-                  <Card {...feed} />
-                </motion.li>
-              ))}
-            </AnimatePresence>
-          </ul>
 
-          {isShowEmpty && (
-            <div className='flex items-center justify-center p-10 text-blue-400'>
-              작성된 에피그램이 없습니다.
-            </div>
-          )}
+            {isShowLoader && (
+              <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
+                <Spinner className='fill-black text-gray-100' />
+                에피그램을 가져오는 중입니다.
+              </div>
+            )}
 
-          {isShowLoader && (
-            <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
-              <Spinner className='fill-black text-gray-100' />
-              에피그램을 가져오는 중입니다.
-            </div>
-          )}
+            {isShowEnd && (
+              <div className='flex items-center justify-center p-10 text-blue-400'>
+                모든 에피그램을 불러왔습니다.
+              </div>
+            )}
+          </div>
 
-          {isShowEnd && (
-            <div className='flex items-center justify-center p-10 text-blue-400'>
-              모든 에피그램을 불러왔습니다.
+          {isShowMoreTrigger && (
+            <div className='pointer-events-none fixed bottom-0 left-0 z-10 w-full md:relative md:bottom-auto md:left-auto'>
+              <div className='from-bg to-bg/10 h-20 bg-gradient-to-t md:hidden'></div>
+              <div className='bg-bg pointer-events-auto flex items-center justify-center pb-15'>
+                <EtcButton size='lg' variant='outlined' onClick={fetchNextPage}>
+                  <Icon name='plus' /> 에피그램 더보기
+                </EtcButton>
+              </div>
             </div>
           )}
         </div>
-
-        {isShowMoreTrigger && (
-          <div className='pointer-events-none fixed bottom-0 left-0 z-10 w-full md:relative md:bottom-auto md:left-auto'>
-            <div className='from-bg to-bg/10 h-20 bg-gradient-to-t md:hidden'></div>
-            <div className='bg-bg pointer-events-auto flex items-center justify-center pb-15'>
-              <EtcButton size='lg' variant='outlined' onClick={fetchNextPage}>
-                <Icon name='plus' /> 에피그램 더보기
-              </EtcButton>
-            </div>
-          </div>
-        )}
       </div>
-    </div>
+      <FloatingButtons />
+    </>
   );
 }
