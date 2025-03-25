e00d15b860eff13df314ae694e2753f844706b5d
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-03-25 21:11:29 +0900

    Refactor/149/검색결과개선 (#171)
    * ✨ Feat: 피드페이지 개선
    
    * ✨ Feat: 검색결과페이지 개선

--- src/app/(after-login)/search/_components/SearchResult.tsx
+++ src/app/(after-login)/search/_components/SearchResult.tsx
@@ -12,42 +12,50 @@ interface SearchResultProps {
 
 export default function SearchResult({ keyword }: SearchResultProps) {
   const [ref, inView] = useInView();
-  const { data, fetchNextPage, hasNextPage, isFetching } = useEpigramSearchInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetching, error } = useEpigramSearchInfiniteQuery({
     limit: 6,
     keyword,
   });
+  const results = data?.pages.flatMap((page) => page.list) ?? [];
 
   useEffect(() => {
     if (inView && hasNextPage && !isFetching) fetchNextPage();
   }, [inView, hasNextPage, fetchNextPage, isFetching]);
 
-  const results = data?.pages.flatMap((page) => page.list) ?? [];
+  if (error) {
+    throw new Error('검색결과를 가져오는중 문제가 발생했습니다.');
+  }
+
   const isShowLoader = isFetching;
   const isShowMoreTrigger = !isFetching && hasNextPage;
   const isShowEmpty = keyword && !results.length && !isFetching;
 
-  if (isShowEmpty) {
-    return <div className='p-4 text-center text-blue-400'>검색결과가 없습니다.</div>;
-  }
+  const renderContent = () => {
+    if (isShowEmpty) {
+      return <div className='p-4 text-center text-blue-400'>검색결과가 없습니다.</div>;
+    }
 
-  return (
-    <>
-      <ul>
-        {results.map((item) => (
-          <li key={item.id}>
-            <SearchCard {...item} keyword={keyword} />
-          </li>
-        ))}
-      </ul>
+    return (
+      <>
+        <ul>
+          {results.map((item) => (
+            <li key={item.id}>
+              <SearchCard {...item} keyword={keyword} />
+            </li>
+          ))}
+        </ul>
 
-      {isShowLoader && (
-        <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
-          <Spinner className='fill-black text-gray-100' />
-          검색중입니다.
-        </div>
-      )}
+        {isShowLoader && (
+          <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
+            <Spinner className='fill-black text-gray-100' />
+            검색중입니다.
+          </div>
+        )}
 
-      {isShowMoreTrigger && <div ref={ref}></div>}
-    </>
-  );
+        {isShowMoreTrigger && <div ref={ref}></div>}
+      </>
+    );
+  };
+
+  return <div className='py-4'>{renderContent()}</div>;
 }
