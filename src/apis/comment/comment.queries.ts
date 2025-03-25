6ef2a32f497ac236e7df79f4ad8fdbeba994aba5
LMS10 <ggb7410@naver.com>
2025-03-25 17:04:02 +0900

    ✨ Feat: 댓글 삭제 기능 구현

--- src/apis/comment/comment.queries.ts
+++ src/apis/comment/comment.queries.ts
@@ -1,6 +1,7 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { getComments } from './comment.service';
+import { useInfiniteQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getComments, deleteComment } from './comment.service';
 import { PaginationQueryParams } from '@/types/common';
+import { Comment } from './comment.type';
 
 export const useCommentsInfiniteQuery = (params: Omit<PaginationQueryParams, 'cursor'>) => {
   return useInfiniteQuery({
@@ -9,5 +10,29 @@ export const useCommentsInfiniteQuery = (params: Omit<PaginationQueryParams, 'cu
       getComments({ ...params, cursor: pageParam }),
     initialPageParam: undefined,
     getNextPageParam: (lastPage) => lastPage.nextCursor || undefined,
+  });
+};
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (commentId: Comment['id']) => deleteComment(commentId),
+    onSuccess: (_, commentId) => {
+      queryClient.invalidateQueries({ queryKey: ['userComments'] });
+
+      queryClient.setQueryData<{
+        pages: { list: Comment[]; nextCursor?: number }[];
+      }>(['userComments'], (oldData) => {
+        if (!oldData) return oldData;
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => ({
+            ...page,
+            list: page.list.filter((comment) => comment.id !== commentId),
+          })),
+        };
+      });
+    },
   });
 };
