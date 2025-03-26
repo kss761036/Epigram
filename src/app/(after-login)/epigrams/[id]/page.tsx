2d82fbc6d3712e3794dbcbf8a5937b62d42c5504
chanki.kim <chanki.kim89@gmail.com>
2025-03-26 19:16:19 +0900

    ✨ Feat: 에피그램 상세 페이지 ssr로 변경

--- src/app/(after-login)/epigrams/[id]/page.tsx
+++ src/app/(after-login)/epigrams/[id]/page.tsx
@@ -1,13 +1,37 @@
 import Inner from '@/components/Inner';
 import EpigramDetail from '../_components/EpigramDetail';
 import EpigramComments from '../_components/EpigramComments';
+import { getQueryClient } from '@/utils/getQueryClient';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { axiosServerInstance } from '@/utils/axios';
+import { notFound } from 'next/navigation';
+import { isAxiosError } from 'axios';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const id = Number((await params).id);
+  const queryClient = getQueryClient();
+
+  try {
+    await queryClient.fetchQuery({
+      queryKey: ['epigrams', id],
+      queryFn: async () => {
+        const response = await axiosServerInstance.get(`/epigrams/${id}`);
+        return response.data;
+      },
+    });
+  } catch (error) {
+    if (isAxiosError(error) && error.response?.status === 404) {
+      notFound();
+    }
+
+    throw new Error('게시물을 가져오는데 문제가 생겼습니다.');
+  }
 
   return (
     <div className='bg-bg flex min-h-dvh flex-col gap-8 md:gap-10 lg:gap-12'>
-      <EpigramDetail id={id} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <EpigramDetail id={id} />
+      </HydrationBoundary>
 
       <Inner>
         <EpigramComments id={id} />
