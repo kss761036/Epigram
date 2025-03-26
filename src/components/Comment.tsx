2321a5573a519ca93e06e8bdb3983dacf63fa834
Minseo Lee <90029564+LMS10@users.noreply.github.com>
2025-03-26 13:08:08 +0900

    Feat/176/댓글 프로필모달연결 (#179)
    * ✨ Feat: 댓글 프로필 이미지 클릭 시 프로필 모달 연결
    
    * 🐛 Bug: 모달 fixed 요소 CLS 문제 해결
    
    * ✨ Feat: 댓글 클릭 시 상세페이지로 이동
    
    * 🔨 Refactor: 댓글 수정/삭제 낙관적 업데이트 패턴 개선

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import formatTime from '@/utils/formatTime';
 import Avatar from './Avatar';
+import ProfileModal from './ProfileModal';
 import { cn } from '@/utils/helper';
+import { Epigram } from '@/apis/epigram/epigram.type';
 
 interface CommentProps {
+  epigramId: Epigram['id'];
   content: string;
   writer: {
     image: string;
@@ -17,6 +21,7 @@ interface CommentProps {
 }
 
 export default function Comment({
+  epigramId,
   content,
   writer,
   updatedAt,
@@ -24,9 +29,15 @@ export default function Comment({
   handleEdit,
   handleDelete,
 }: CommentProps) {
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+
+  const handleCommentClick = () => {
+    window.location.href = `/epigrams/${epigramId}`;
+  };
+
   const classes = {
     commentWrapper: cn(
-      'border-line-200 flex items-start border-t px-6 py-4 text-left md:py-6 lg:py-9',
+      'border-line-200 flex items-start border-t px-6 py-4 text-left md:py-6 lg:py-9 cursor-pointer',
       className,
     ),
     commentBox: 'ml-4 flex-1',
@@ -39,33 +50,55 @@ export default function Comment({
   };
 
   return (
-    <div className={classes.commentWrapper}>
-      <Avatar src={writer.image} alt={writer.nickname} />
-      <div className={classes.commentBox}>
-        <div className={classes.commentInfo}>
-          <div className={classes.commentInfoText}>{writer.nickname}</div>
-          <div className={cn(classes.commentInfoText, 'ml-2')}>{formatTime(updatedAt)}</div>
-          <ul className={classes.commentInfoBtns}>
-            <li>
-              <button
-                className={cn(classes.commentInfoBtn, 'text-black-600 decoration-black-600')}
-                onClick={handleEdit}
-              >
-                수정
-              </button>
-            </li>
-            <li>
-              <button
-                className={cn(classes.commentInfoBtn, 'text-red decoration-red')}
-                onClick={handleDelete}
-              >
-                삭제
-              </button>
-            </li>
-          </ul>
+    <>
+      <div onClick={handleCommentClick} className={classes.commentWrapper}>
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsProfileModalOpen(true);
+          }}
+          className='cursor-pointer'
+        >
+          <Avatar src={writer.image} alt={writer.nickname} />
         </div>
-        <div className={classes.commentContent}>{content}</div>
+        <div className={classes.commentBox}>
+          <div className={classes.commentInfo}>
+            <div className={classes.commentInfoText}>{writer.nickname}</div>
+            <div className={cn(classes.commentInfoText, 'ml-2')}>{formatTime(updatedAt)}</div>
+            <ul className={classes.commentInfoBtns}>
+              <li>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEdit?.();
+                  }}
+                  className={cn(classes.commentInfoBtn, 'text-black-600 decoration-black-600')}
+                >
+                  수정
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDelete?.();
+                  }}
+                  className={cn(classes.commentInfoBtn, 'text-red decoration-red')}
+                >
+                  삭제
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className={classes.commentContent}>{content}</div>
+        </div>
       </div>
-    </div>
+
+      <ProfileModal
+        isOpen={isProfileModalOpen}
+        writer={writer}
+        onClose={() => setIsProfileModalOpen(false)}
+      />
+    </>
   );
 }
