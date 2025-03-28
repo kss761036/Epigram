0cba3d7f17596b0185a2c63cd0d9452554514771
Minseo Lee <90029564+LMS10@users.noreply.github.com>
2025-03-28 17:29:01 +0900

    Refactor/마이페이지 Inner컴포넌트적용 (#202)
    * 🔨 Refactor: 마이페이지 Inner 컴포넌트 적용
    
    * 🔨 Refactor: 에피그램 생성 페이지 경로 수정

--- src/app/(after-login)/mypage/page.tsx
+++ src/app/(after-login)/mypage/page.tsx
@@ -4,6 +4,7 @@ import MyProfile from './_components/MyProfile';
 import TodayMood from '@/components/TodayMood';
 import MonthlyLogs from './_components/MonthlyLogs';
 import MyWritings from './_components/MyWritings';
+import Inner from '@/components/Inner';
 import { format } from 'date-fns';
 
 export default function Page() {
@@ -14,19 +15,18 @@ export default function Page() {
       <MyProfile />
       <div className='bg-bg pt-16 lg:pt-32'>
         <div className='rounded-3xl bg-blue-100 pt-[184px] pb-10 shadow-[0px_0px_36px_0px_rgba(0,_0,_0,_0.05)] md:pb-[63px] lg:pt-[276px] lg:pb-[88px]'>
-          <div className='mx-auto w-full max-w-[312px] md:max-w-[384px] lg:max-w-[640px]'>
+          <Inner>
             <div className='flex justify-between'>
               <p className='text-black-600 text-lg font-semibold lg:text-2xl'>오늘의 감정</p>
               <span className='text-lg text-blue-400 lg:text-xl'>{today}</span>
             </div>
             <TodayMood />
-
             <MonthlyLogs />
-          </div>
+          </Inner>
         </div>
-        <div className='mx-auto mt-14 w-full max-w-[312px] pb-[114px] md:max-w-[384px] md:pb-[233px] lg:mt-24 lg:max-w-[640px] lg:pb-72'>
+        <Inner className='mt-14 pb-[114px] md:pb-[233px] lg:mt-24 lg:pb-72'>
           <MyWritings />
-        </div>
+        </Inner>
       </div>
     </>
   );
