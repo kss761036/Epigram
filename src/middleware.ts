0d9724793f3880abda780eaf061fb8cdb2257c0a
chanki.kim <chanki.kim89@gmail.com>
2025-03-28 11:55:23 +0900

    🔨 Refactor: middleware 개선

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
-  return NextResponse.next();
-}
+export { default } from 'next-auth/middleware';
 
 export const config = {
   matcher: [
