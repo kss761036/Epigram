6ee793daa097b50b3ce93c044aad640898238e54
chanki.kim <chanki.kim89@gmail.com>
2025-03-28 11:57:04 +0900

    🔨 Refactor: 불필요한 matcher 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: [
-    '/addepigram/:path*',
-    '/epigrams/:path*',
-    '/mypage/:path*',
-    '/search/:path*',
-    '/feeds/:path*',
-  ],
+  matcher: ['/epigrams/:path*', '/mypage/:path*', '/search/:path*', '/feeds/:path*'],
 };
