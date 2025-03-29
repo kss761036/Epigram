2283c1a13d69d6c03673fd5a69e7c91f88346ff7
chanki.kim <chanki.kim89@gmail.com>
2025-03-29 23:15:37 +0900

    ✨ Feat: api proxy 코드 개선

--- src/app/api/[...endpoint]/route.ts
+++ src/app/api/[...endpoint]/route.ts
@@ -2,14 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import axios, { Method } from 'axios';
 import { axiosServerInstance } from '@/utils/axios';
 
+const METHODS_WITHOUT_BODY = ['GET', 'DELETE', 'HEAD', 'OPTIONS'];
+const NO_BODY_STATUS = [204, 205];
+
 async function handler(req: NextRequest, method: Method) {
   const url = `${req.nextUrl.pathname.replace('/api', '')}${req.nextUrl.search}`;
-  const isFormData = req.headers.get('content-type')?.includes('multipart/form-data');
-  const isBodyAllowed = !['GET', 'DELETE', 'HEAD', 'OPTIONS'].includes(method.toUpperCase());
-
-  const body = isFormData
-    ? await req.formData().catch(() => undefined)
-    : await req.json().catch(() => ({}));
+  const isBodyAllowed = !METHODS_WITHOUT_BODY.includes(method.toUpperCase());
+  const body = await parseRequestBody(req);
 
   try {
     const response = await axiosServerInstance({
@@ -18,7 +17,16 @@ async function handler(req: NextRequest, method: Method) {
       ...(isBodyAllowed && { data: body }),
     });
 
-    return NextResponse.json(response.data, { status: response.status });
+    const status = response.status;
+    const shouldReturnEmptyBody = NO_BODY_STATUS.includes(status);
+
+    if (shouldReturnEmptyBody) {
+      return new NextResponse(null, { status });
+    }
+
+    return NextResponse.json(response.data, {
+      status,
+    });
   } catch (error) {
     if (axios.isAxiosError(error)) {
       return NextResponse.json({ error: error.message }, { status: error.response?.status || 500 });
@@ -45,4 +53,25 @@ export async function PATCH(req: NextRequest) {
 
 export async function DELETE(req: NextRequest) {
   return handler(req, 'DELETE');
+}
+
+export async function parseRequestBody(req: NextRequest): Promise<unknown> {
+  const contentType = req.headers.get('content-type') || '';
+
+  try {
+    if (contentType.includes('application/json')) {
+      return await req.json();
+    }
+
+    if (
+      contentType.includes('multipart/form-data') ||
+      contentType.includes('application/x-www-form-urlencoded')
+    ) {
+      return await req.formData();
+    }
+
+    return await req.text();
+  } catch {
+    return undefined;
+  }
 }
