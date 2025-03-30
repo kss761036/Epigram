a8d7d13996adfa9e561b9787e2e61dad49f29165
chanki.kim <chanki.kim89@gmail.com>
2025-03-30 13:17:48 +0900

    💄 Design: 상세페이지 애니메이션 효과 추가

--- src/app/(after-login)/epigrams/_components/detail/DetailWrapper.tsx
+++ src/app/(after-login)/epigrams/_components/detail/DetailWrapper.tsx
@@ -1,17 +1,24 @@
 import { PropsWithChildren } from 'react';
+import { motion } from 'motion/react';
 import Inner from '@/components/Inner';
 import paperBackground from '@/assets/img/common/paper.svg';
 
 export function DetailWrapper({ children }: PropsWithChildren) {
   return (
-    <section>
+    <motion.section
+      initial={{ y: -300 }}
+      animate={{ y: 0 }}
+      transition={{ ease: 'easeOut', duration: 0.2 }}
+    >
       <div className='bg-[repeating-linear-gradient(#fff_0_36.69px,#f2f2f2_0_37.69px)] py-10'>
-        <Inner>{children}</Inner>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2 }}>
+          <Inner>{children}</Inner>
+        </motion.div>
       </div>
       <div
         className='h-10 bg-repeat-x'
         style={{ backgroundImage: `url(${paperBackground.src})` }}
       ></div>
-    </section>
+    </motion.section>
   );
 }
