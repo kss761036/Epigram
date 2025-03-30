411b8f012765b1c3d946f40e780b3a47722c6895
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-03-31 08:46:45 +0900

    Feat/213/상세페이지개선 (#215)
    * 💄 Design: 로딩 컴포넌트 여백 수정
    
    * 💄 Design: 출처 ux개선, 아이콘 크기 통일
    
    * ✨ Feat: 상세페이지 csr로 재전환 및 컨테이너 컴포넌트를 page로 이동
    
    * 💄 Design: 상세페이지 애니메이션 효과 추가
    
    * 🔨 Refactor: import 순서 정리

--- src/app/(after-login)/epigrams/_components/detail/DetailLoading.tsx
+++ src/app/(after-login)/epigrams/_components/detail/DetailLoading.tsx
@@ -2,7 +2,7 @@ import Spinner from '@/components/Spinner';
 
 export function DetailLoading() {
   return (
-    <div className='flex flex-col items-center justify-center gap-4 p-4 text-center text-blue-400'>
+    <div className='flex flex-col items-center justify-center gap-4 p-10 pt-24 text-center text-blue-400'>
       <Spinner className='fill-black text-gray-100' />
       게시물을 가져오는 중입니다.
     </div>
