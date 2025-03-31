68d78e3438dbee4a921fa5e2ad74b06c97299759
chanki.kim <chanki.kim89@gmail.com>
2025-03-31 18:04:55 +0900

    ✨ Feat: cypress login command 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'cypress';
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(process.cwd());
 
 export default defineConfig({
+  env: {
+    database_url: process.env.NEXT_PUBLIC_BASE_URL,
+    test_email: process.env.CY_TEST_EMAIL,
+    test_password: process.env.CY_TEST_PW,
+  },
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
