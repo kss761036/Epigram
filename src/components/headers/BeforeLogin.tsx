9e525f687e673f638ebf1e42d7cdbb302cc68bf0
김승석 <kss53043@naver.com>
2025-04-05 21:54:21 +0900

    🔨 Refactor: 로그인 체크 세션으로 변경

--- src/components/headers/BeforeLogin.tsx
+++ src/components/headers/BeforeLogin.tsx
@@ -1,14 +1,13 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '@/assets/img/common/logo.svg';
-import { useAuth } from '@/hooks/useAuth';
 import Icon from '../Icon';
 
 export default function BeforeLoginHeader() {
-  const { isAuthenticated } = useAuth();
-
+  const { data: session } = useSession();
   return (
     <header className='border-line-100 fixed top-0 left-0 z-50 h-13 w-full border-b bg-white md:h-15.5 lg:h-20'>
       <div className='flex h-full items-center justify-between px-6 md:px-19'>
@@ -20,7 +19,7 @@ export default function BeforeLoginHeader() {
             <Image src={Logo} alt='epigram' className='h-6.5 w-auto lg:h-9' />
           </Link>
         </h1>
-        <Link href={isAuthenticated ? '/mypage' : '/login'}>
+        <Link href={session ? '/mypage' : '/login'}>
           <Icon name='person' size={20} className='lg:w-[30px]' />
         </Link>
       </div>
