bd686431e08e3fd13101c05ce2583f47fe617241
kss761036 <kss761036@gmail.com>
2025-04-07 16:16:58 +0900

    🔥 Remove: 중복된 reset 제거

--- src/app/(after-login)/epigrams/create/page.tsx
+++ src/app/(after-login)/epigrams/create/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
-import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import { createEpigram } from '@/apis/epigram/epigram.service';
@@ -15,9 +14,6 @@ export default function Page() {
   const { mutateAsync, isPending } = useMutation({
     mutationFn: createEpigram,
   });
-  const methods = useForm();
-  const { reset } = methods;
-
   return (
     <Inner className='p-6 lg:py-8'>
       <Section>에피그램 만들기</Section>
@@ -28,7 +24,6 @@ export default function Page() {
           try {
             const result = await mutateAsync(data);
             toast.success('에피그램이 작성되었습니다.');
-            reset();
             router.push(`/epigrams/${result.id}`);
           } catch (error) {
             console.error('에피그램 작성 실패:', error);
